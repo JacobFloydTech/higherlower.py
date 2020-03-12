a53ae3cfb3d09c321344dda7336f64d12f1e5a10
Jecub <41782145+Jecub@users.noreply.github.com>
2020-03-13 11:56:30 +1300

    Added max secret number value customization

--- higher-lower.py
+++ higher-lower.py
@@ -4,13 +4,13 @@ import os
 import sys
 import time
 listofnumbers = []
+
+
 def clear():
     if platform.system() == 'Windows':
         os.system('cls')
     elif platform.system() == "Linux":
         os.system('clear')
-
-number = random.randint(1,100)
 
 def type(string):
   for char in string:  
@@ -21,17 +21,27 @@ def type(string):
 def start():
     clear()
     type("Welcome! \n")
-    type("Do you want the rounds to be limited or to count you? Please print 1 for limited and 2 for unlimited rounds:  \n")
+    type("Do you want the rounds to be limited or to count you? Please type 1 for limited and 2 for unlimited rounds:  \n")
     option = input("")
     if option == '1':
         option1()
     elif option == '2':
         option2()
-
+    elif option != '1' or option != '2':
+        type("Please either use 1 or 2") 
+        option = input("")
 
 def option1():
     try:
+        type("Do you want to set a max value for the number?")
+        optionx = input("")
+        if optionx.lower() == 'y':
+            optiony = input("Number : ")
+            secret_number = random.randint(1, int(optiony))
+        elif optionx.lower() == 'n':
+            secret_number = random.randint(1,100)
         rounds = 10
+        print(secret_number)
         guesses = 1
         while rounds > 0:
             guess = input("Number ")
@@ -41,28 +51,30 @@ def option1():
                 guess = input("Number ")
             elif int(guess) not in listofnumbers:
                 listofnumbers.append(int(guess))
-                if int(guess) == number:
+                if int(guess) == secret_number:
                     print("Won")
                     if guesses == 1:
                         print("You did it in 1 round!")
                     else:
                         print("You did it in %s rounds" % guesses)
                     break
-                elif int(guess) > number:
+                elif int(guess) > secret_number:
                     clear()
-                    print("too high")
+                    print("Too high")
                     rounds -= 1
                     guesses += 1 
                     print("You have %s rounds left" % rounds)
-                elif int(guess) < number:
+                elif int(guess) < secret_number:
                     clear()
                     print("Too low")
                     rounds -= 1
                     guesses += 1
                     print("You have %s rounds left" % rounds)
-        print("Number to get was " + str(number))
+        print("Number to get was " + str(secret_number))
         play_again = input("Play again? Y/N")
         if play_again.upper() == 'Y':
+            clear()
+            listofnumbers.clear()
             start()
         elif play_again.upper() == "N":
             sys.exit()
@@ -77,9 +89,17 @@ def option1():
 
 def option2():
     try:
-        guesses = 0
+        type("Do you want to set a max value for the number?")
+        optionx = input("")
+        if optionx.lower() == 'y':
+            optiony = input("Number : ")
+            secret_number = random.randint(1, int(optiony))
+        elif optionx.lower() == 'n':
+            secret_number = random.randint(1,100)
+        guesses = 1
         rounds = 1
         clear()
+        print(secret_number)
         while rounds == 0 or rounds > 0:
             print("Round %s" % str(rounds))
             guess = input("Number ")
@@ -88,23 +108,24 @@ def option2():
                 print(listofnumbers)
             elif int(guess) not in listofnumbers:
                 listofnumbers.append(int(guess))
-                if int(guess) > number:
+                if int(guess) > secret_number:
                     clear()
                     print("Too high")
                     rounds += 1
                     guesses += 1
-                elif int(guess) < number:
+                elif int(guess) < secret_number:
                     clear()
                     print("Too low")
                     rounds += 1
                     guesses += 1
-                elif int(guess) == number:
+                elif int(guess) == secret_number:
                     clear()
-                    print("You got it! It was " + str(number))
+                    print("You got it! It was " + str(secret_number))
                     print("You did it in %s guesses" % guesses)
                     playagain = input("Do you wanna play again?")
                     if playagain.lower() == 'yes' or playagain.lower() == 'y':
                         clear()
+                        listofnumbers.clear()
                         start()
                     elif playagain.lower() == 'no' or playagain.lower() == 'n':
                         sys.exit()
